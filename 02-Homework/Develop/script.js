5abae5e4f49632382a52b55e28bab5f427015851
E Alexander <erick.interiano05@gmail.com>
2021-02-11 21:16:16 -0800

    finished making the different arrays

--- 02-Homework/Develop/script.js
+++ 02-Homework/Develop/script.js
@@ -1,6 +1,11 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+var numbers = ["0","1","2","3","4","5","6","7","8","9"];
+var specialCharacters = ["!","@","#","$","%","^","&","*","(",")"];
+var upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","P","Q","R","S","T","U","V","W","X","Y","Z"];
+VAR lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
